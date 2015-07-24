d9c4b1bf0f2ddb3140e537abf958badb70eed5bb
Onlife KK <Krishna_Kotte@onlifehealth.com>
2015-07-24 13:15:23 +0530

    POC for testing Push Notification using Pushsharp Library - Initial Check Out

---
+++ PushNotifPOC/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PushNotifPOC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSend.Location = new System.Drawing.Point(221, 274);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(142, 39);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send Notification";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(16, 87);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(553, 158);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.Text = "Hello World , this is a sample test notificatiion from net";
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStart.Location = new System.Drawing.Point(270, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(142, 39);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Service";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStop.Location = new System.Drawing.Point(427, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(142, 39);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop Service";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 386);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Send Notifications";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ PushNotifPOC/Form1.cs
@@ -0,0 +1,156 @@
+﻿using PushSharp;
+using PushSharp.Apple;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PushNotifPOC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtMessage.Text.Length > 0)
+            {
+                try
+                {
+                    string deviceId = "4ead049e00cc26141740ca902c0a0e7e6d11b9b52b6c215ce9654cf3a60c2225";
+                    string appLocation = System.AppDomain.CurrentDomain.BaseDirectory;
+                    var appleCert = File.ReadAllBytes(string.Concat(appLocation, @"..\..\certs\INT.p12"));
+                    //IMPORTANT: If you are using a Development provisioning Profile, you must use
+                    // the Sandbox push notification server 
+                    //  (so you would leave the first arg in the ctor of ApplePushChannelSettings as
+                    // 'false')
+                    //  If you are using an AdHoc or AppStore provisioning profile, you must use the 
+                    //Production push notification server
+                    //  (so you would change the first arg in the ctor of ApplePushChannelSettings to 
+                    //'true')
+                    push.RegisterAppleService(new ApplePushChannelSettings(false, appleCert, ""));
+                    //Extension method
+                    //Fluent construction of an iOS notification
+                    //IMPORTANT: For iOS you MUST MUST MUST use your own DeviceToken here that gets
+                    // generated within your iOS app itself when the Application Delegate
+                    //  for registered for remote notifications is called, 
+                    // and the device token is passed back to you
+                    push.QueueNotification(new AppleNotification()
+                                                .ForDeviceToken(deviceId)//the recipient device id
+                                                .WithAlert(txtMessage.Text)//the message
+                                                .WithBadge(1)
+                                                .WithSound("sound.caf")
+                                                );
+
+                    stopActions();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter a message to send notification");
+            }
+        }
+
+        void push_OnChannelDestroyed(object sender)
+        {
+            //stopActions();
+            MessageBox.Show("push_OnChannelDestroyed");
+        }
+
+        void push_OnChannelCreated(object sender, PushSharp.Core.IPushChannel pushChannel)
+        {
+            MessageBox.Show("push_OnChannelCreated");
+        }
+
+        void push_OnDeviceSubscriptionChanged(object sender, string oldSubscriptionId, string newSubscriptionId, PushSharp.Core.INotification notification)
+        {
+            //stopActions();
+            MessageBox.Show("push_OnDeviceSubscriptionChanged");
+        }
+
+        void push_OnDeviceSubscriptionExpired(object sender, string expiredSubscriptionId, DateTime expirationDateUtc, PushSharp.Core.INotification notification)
+        {
+            //stopActions();
+            MessageBox.Show("OnDeviceSubscriptionExpired : " + expiredSubscriptionId);
+        }
+
+        void push_OnNotificationFailed(object sender, PushSharp.Core.INotification notification, Exception error)
+        {
+            //stopActions();
+            MessageBox.Show("OnNotificationFailed : " + error);
+        }
+
+        void push_OnServiceException(object sender, Exception error)
+        {
+            //stopActions();
+            MessageBox.Show("OnServiceException : " + error);
+        }
+
+        void push_OnChannelException(object sender, PushSharp.Core.IPushChannel pushChannel, Exception error)
+        {
+            //stopActions();
+            MessageBox.Show("OnChannelException : " + error);
+        }
+
+        void push_OnNotificationSent(object sender, PushSharp.Core.INotification notification)
+        {
+            MessageBox.Show("push_OnNotificationSent");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            push = new PushBroker();
+            push.OnNotificationSent += push_OnNotificationSent;
+            push.OnChannelException += push_OnChannelException;
+            push.OnServiceException += push_OnServiceException;
+            push.OnNotificationFailed += push_OnNotificationFailed;
+            push.OnDeviceSubscriptionExpired += push_OnDeviceSubscriptionExpired;
+            push.OnDeviceSubscriptionChanged += push_OnDeviceSubscriptionChanged;
+            push.OnChannelCreated += push_OnChannelCreated;
+            push.OnChannelDestroyed += push_OnChannelDestroyed;
+            startActions();
+        }
+
+        private void stopActions()
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnSend.Enabled = false;
+            push.StopAllServices(waitForQueuesToFinish: true);
+        }
+
+        private void startActions()
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnSend.Enabled = true;
+        }
+
+        PushBroker push;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnSend.Enabled = false;
+            push.StopAllServices(waitForQueuesToFinish: true);
+        }
+    }
+}
